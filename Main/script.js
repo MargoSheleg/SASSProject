d634c3ebad40ea2122691fc7012de07fbe324f5b
Margarita.Sheleg <margosheleg23@gmail.com>
2023-11-11 17:16:57 +0100

    almost finished the first part of the project

--- Main/script.js
+++ Main/script.js
@@ -3,3 +3,8 @@ const author = document.querySelector("#author");
 author.addEventListener("click", () => {
   window.location.href = "/Author/author.html";
 });
+
+const aHome = document.querySelector("#home");
+if ((window.location.href = "/Main/main.html")) {
+  aHome.style.color = "#FFCA42";
+}
